05f678451d1d0176f2fb607285f93e001f8e4a2a
AakankshaShirodkarQA <aakankshashirodkar1409@gmail.com>
2025-07-04 23:42:09 +0530

    changed project name as it was incorrect previously

---
+++ src/ex_02_Java_Basics/Lab024_TaskToWriteTable.java
@@ -0,0 +1,40 @@
+package ex_02_Java_Basics;
+
+public class Lab024_TaskToWriteTable {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 1;
+        int b1 = 2;
+        int b2 = 3;
+        int b3 = 4;
+        int b4 = 5;
+        int b5 = 6;
+        int b6 = 7;
+        int b7 = 8;
+        int b8 = 9;
+        int b9 = 10;
+
+
+        System.out.println("Table of 9");
+        System.out.printf("%d x %d = %d", a,b,a);
+        System.out.println();
+        System.out.printf("%d x %d = %d", a,b1,a*b1);
+        System.out.println();
+        System.out.printf("%d x %d = %d", a,b,a*b2);
+        System.out.println();
+        System.out.printf("%d x %d = %d", a,b,a*b3);
+        System.out.println();
+        System.out.printf("%d x %d = %d", a,b,a*b4);
+        System.out.println();
+        System.out.printf("%d x %d = %d", a,b,a*b5);
+        System.out.println();
+        System.out.printf("%d x %d = %d", a,b,a*b6);
+        System.out.println();
+        System.out.printf("%d x %d = %d", a,b,a*b7);
+        System.out.println();
+        System.out.printf("%d x %d = %d", a,b,a*b8);
+        System.out.println();
+        System.out.printf("%d x %d = %d", a,b,a*b9);
+
+    }
+}
